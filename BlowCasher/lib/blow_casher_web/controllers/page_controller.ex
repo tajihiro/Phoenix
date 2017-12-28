8b59ae63f2ce9d67ce5ba1857c81b9ca39406f7f
H.Tajima <tajima@blowfish.co.jp>
2017-12-28 17:44:06 +0900

    Modified select tag for Items in PageController.

--- BlowCasher/lib/blow_casher_web/controllers/page_controller.ex
+++ BlowCasher/lib/blow_casher_web/controllers/page_controller.ex
@@ -3,9 +3,11 @@ defmodule BlowCasherWeb.PageController do
 
   alias BlowCasher.Casher
   alias BlowCasher.Casher.Group
+  alias BlowCasher.Casher.Item
 
   def index(conn, %{"crypto_id" => crypto_id}) do
     group = BlowCasher.Repo.get_by!(Group, crypto_id: crypto_id)
-    render(conn, "index.html", group: group)
+    items = Casher.list_items_by_crypto_id(crypto_id)
+    render(conn, "index.html", [group: group, items: items])
   end
 end
