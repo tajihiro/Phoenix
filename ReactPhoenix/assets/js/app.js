d833934f640aac75303c1ec609e3371fce8083ca
Hiroyuki Tajima <tajima@blowfish.co.jp>
2019-11-27 17:59:50 +0900

    Use redux modules.

--- ReactPhoenix/assets/js/app.js
+++ ReactPhoenix/assets/js/app.js
@@ -1,5 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import Main from './Main'
 
-const MyReactComponent = () => <div>こんにちは。React!!</div>
-ReactDOM.render(<MyReactComponent />, document.getElementById('root'))
+let state_value = {
+    counter : 0,
+    message : "COUNTER"
+}
+
+function counter(state = state_value, action){
+    return state;
+}
+
+let store = createStore(counter);
+
+ReactDOM.render(
+    <Provider store={store} >
+        <Main/>
+    </Provider>,
+    document.getElementById('root')
+);
