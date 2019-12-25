5e455f74202c6f17cb45952149273d4ef3320717
H.Tajima <hiroyuki.tajima@pia.co.jp>
2019-12-25 15:41:23 +0900

    Teams Schema 作成

--- ReactPhoenixRedux/test/react_phoenix_redux/redux_test.exs
+++ ReactPhoenixRedux/test/react_phoenix_redux/redux_test.exs
@@ -67,4 +67,63 @@ defmodule ReactPhoenixRedux.ReduxTest do
       assert %Ecto.Changeset{} = Redux.change_member(member)
     end
   end
+
+  describe "teams" do
+    alias ReactPhoenixRedux.Redux.Team
+
+    @valid_attrs %{team_name: "some team_name"}
+    @update_attrs %{team_name: "some updated team_name"}
+    @invalid_attrs %{team_name: nil}
+
+    def team_fixture(attrs \\ %{}) do
+      {:ok, team} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Redux.create_team()
+
+      team
+    end
+
+    test "list_teams/0 returns all teams" do
+      team = team_fixture()
+      assert Redux.list_teams() == [team]
+    end
+
+    test "get_team!/1 returns the team with given id" do
+      team = team_fixture()
+      assert Redux.get_team!(team.id) == team
+    end
+
+    test "create_team/1 with valid data creates a team" do
+      assert {:ok, %Team{} = team} = Redux.create_team(@valid_attrs)
+      assert team.team_name == "some team_name"
+    end
+
+    test "create_team/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Redux.create_team(@invalid_attrs)
+    end
+
+    test "update_team/2 with valid data updates the team" do
+      team = team_fixture()
+      assert {:ok, %Team{} = team} = Redux.update_team(team, @update_attrs)
+      assert team.team_name == "some updated team_name"
+    end
+
+    test "update_team/2 with invalid data returns error changeset" do
+      team = team_fixture()
+      assert {:error, %Ecto.Changeset{}} = Redux.update_team(team, @invalid_attrs)
+      assert team == Redux.get_team!(team.id)
+    end
+
+    test "delete_team/1 deletes the team" do
+      team = team_fixture()
+      assert {:ok, %Team{}} = Redux.delete_team(team)
+      assert_raise Ecto.NoResultsError, fn -> Redux.get_team!(team.id) end
+    end
+
+    test "change_team/1 returns a team changeset" do
+      team = team_fixture()
+      assert %Ecto.Changeset{} = Redux.change_team(team)
+    end
+  end
 end
